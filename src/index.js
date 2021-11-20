f85e3aebaa23a0a18c80bd112305dc3b56457775
Pru <87931775+IPIIXIEEL@users.noreply.github.com>
2021-11-20 19:06:42 +1100

    forecast array and day names updated

--- src/index.js
+++ src/index.js
@@ -39,34 +39,43 @@ function formatDate(timestamp) {
 
 //forecast testing
 //forecastContainerUpdate
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return days[day];
+}
+
 function displayForecast(response) {
     console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-    ];
-    days.forEach(function(day) {
-        forecastHTML =
-            forecastHTML +
-            `<div class="col-2">
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML +
+                `<div class="col-2">
                                 <div class="card text-dark bg-warning mb-3" style="max-width: 18rem;">
-                                    <div class="weather-forecast-date">${day}</div>
-
+                                    <div class="weather-forecast-date">${formatDay(
+                                      forecastDay.dt
+                                    )}</div>
                                     <div class="weather-forecast-temp" id="mon">
-                                        <span id="weather-forecast-min">7777°C</span>
-                                        <span id="weather-forecast-max">12°C</span>
-                                        <img src="media/weather_icon.svg" id="icon" alt="sun" />
+                                        <span id="weather-forecast-min">${Math.round(
+                                          forecastDay.temp.max
+                                        )}°C</span>
+                                        <span id="weather-forecast-max">${Math.round(
+                                          forecastDay.temp.min
+                                        )}°C </span>
+                                        <img src="http://openweathermap.org/img/wn/${
+                                          forecastDay.weather[0].icon
+                                        }@2x.png" id="icon" alt="sun" />
                                     </div>
                                 </div>
                             </div>`;
+        }
     });
 
     forecastHTML = forecastHTML + `</div>`;
